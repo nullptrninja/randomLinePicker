f6c4bd2cc26fbf520ca4c7c5b819367e615814b9
Brian Li <lordrider@gmail.com>
2018-11-12 23:04:35 -0500

    Added script and sample

---
+++ pickFromFile.ps1
@@ -0,0 +1,40 @@
+﻿param(
+    [string]$file = $null,
+    [int]$pick = 3
+)
+
+if ($file -eq $null -or $pick -le 0) {
+    write-host "Bad params";
+    exit;
+}
+
+if ((test-path $file) -eq $false) {
+    write-host "Can't find file $file";
+    exit;
+}
+
+$fullPath = $(Get-ItemProperty -Path $file).FullName;
+$lines = [System.IO.File]::ReadAllLines($fullPath);
+$actualPick = $pick;
+
+if ($lines.Length -le $pick) {
+    write-host "There were less lines in the file than you picked, showing all lines...";
+    $actualPick = $lines.Length;
+}
+
+$alreadyPicked = new-object System.Collections.Generic.HashSet[string];
+$i = 1;
+
+while ($i -le $actualPick) {
+    $index = get-random -Minimum 0 -Maximum $lines.Length
+    $pickedName = $lines[$index];
+
+    if ($alreadyPicked.Contains($pickedName) -eq $false) {
+        write-host "Picked index: $i -> $pickedName"
+
+        # Supress the return value
+        $derp = $alreadyPicked.Add($pickedName);
+        $i++;
+    }
+    
+}
